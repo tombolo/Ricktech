dc85fadcc44ae7796192b1baccba0d2d963c124d
tomboloski <ndanumumo93@gmail.com>
2023-07-17 18:59:07 +0300

    add redirect for deployment

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
 
 
 module.exports = {
   async redirects() {
     return [
       {
-        source: '/about',
+        source: '/',
         destination: '/',
         permanent: true,
       },
