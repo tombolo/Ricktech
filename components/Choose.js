6487308fcdf347e03649a00e42fac284b82dade8
tomboloski <ndanumumo93@gmail.com>
2023-07-22 00:52:32 +0300

    day 5

--- components/Choose.js
+++ components/Choose.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import Image from 'next/image';
 import Center from "@/components/Center";
+import Image1 from '../components/images/good1.jpg';
+import Image2 from '../components/images/good5.jpg';
+import Image3 from '../components/images/good6.jpg';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 const StyledSection = styled.section`
@@ -14,10 +19,10 @@ const StyledSection = styled.section`
   width: 100%;
   
   @media screen and (max-width: 768px) {
-    margin-left: 0px;;
+    margin-left: 5px;
   }
   @media screen and (min-width: 768px) {
-    margin-right: 0px;;
+    margin-right: 0px;
   }
 `;
 
@@ -58,14 +63,28 @@ export default function Choose() {
     <StyledSection>
       
       <ChooseWrapper>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={3000}
+      >
         <ChooseImageContainer>
-
+        <StyledImage src={Image1} alt="Image 1" layout="fill" objectFit="cover" />
         </ChooseImageContainer>
-      </ChooseWrapper>
-      <ChooseWrapper>
-        <ChooseImageContainer2>
-          
-        </ChooseImageContainer2>
+
+        <ChooseImageContainer>
+        <StyledImage src={Image2} alt="Image 1" layout="fill" objectFit="cover" />
+        </ChooseImageContainer>
+
+        <ChooseImageContainer>
+        <StyledImage src={Image3} alt="Image 1" layout="fill" objectFit="cover" />
+        </ChooseImageContainer>
+ 
+        </Carousel>
+
       </ChooseWrapper>
     </StyledSection>
 
