07637026ff90926dfe4283de1dd391f3766439d4
tomboloski <ndanumumo93@gmail.com>
2023-07-24 04:26:13 +0300

    day 8

--- pages/search.js
+++ pages/search.js
@@ -6,10 +6,7 @@ import { Product } from "@/models/Product";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const StyledSection = styled.section`
-
-`;
-
+const StyledSection = styled.section``;
 
 const SearchResultsWrapper = styled.div`
   display: flex;
@@ -17,7 +14,7 @@ const SearchResultsWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 96%;
-  margin: 0 auto; 
+  margin: 0 auto;
   padding-top: 140px;
 `;
 
@@ -26,20 +23,26 @@ const SearchResultItem = styled.div`
   border: 1px solid gray;
   border-radius: 4px;
   margin: 5px;
+
+  /* Add hover effect */
+  &:hover {
+    background-color: lightgray;
+    cursor: pointer;
+  }
 `;
 
 const Title = styled.h3`
-  font-size: 12px;
+  font-size: 13px;
   font-weight: bold;
   color: blue;
-  margin-bottom: -7px; 
+  margin-bottom: -7px;
 `;
 
 const Description = styled.p`
-  font-size: 10px;
+  font-size: 11px;
   color: black;
   display: -webkit-box;
-  -webkit-line-clamp: 1; /* Show only one line of description */
+  -webkit-line-clamp: 1;
   -webkit-box-orient: vertical;
   overflow: hidden;
 `;
@@ -48,26 +51,35 @@ export default function Search({ searchResults }) {
   const router = useRouter();
   const { q } = router.query;
 
+  // Define a function to handle click on a search result
+  const handleSearchResultClick = (productId) => {
+    // Redirect to the product page (you need to define the route for the product page)
+    router.push(`/products/${productId}`);
+  };
+
   return (
     <>
-    <StyledSection>
+      <StyledSection>
         <Header />
       </StyledSection>
-      
-    <SearchResultsWrapper>
-    <h2>Search Results for {q}</h2>
-      {searchResults.length === 0 ? (
-        <p>No results found</p>
-      ) : (
-        searchResults.map((product) => (
-          <SearchResultItem key={product._id}>
-            <Title>{product.title}</Title>
-            <Description>{product.description}</Description>
-          </SearchResultItem>
-        ))
-      )}
-    </SearchResultsWrapper>
-    <Footer />
+
+      <SearchResultsWrapper>
+      <h2 style={{ fontSize: "20px" }}>Search Results for {q}</h2>
+        {searchResults.length === 0 ? (
+          <p>No results found</p>
+        ) : (
+          searchResults.map((product) => (
+            <SearchResultItem
+              key={product._id}
+              onClick={() => handleSearchResultClick(product._id)}
+            >
+              <Title>{product.title}</Title>
+              <Description>{product.description}</Description>
+            </SearchResultItem>
+          ))
+        )}
+      </SearchResultsWrapper>
+      <Footer />
     </>
   );
 }
